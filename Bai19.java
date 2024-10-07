f0d2fa84003661d557282a7cd9d0f76062cc5794
Cao Thanh Tai <taict.24ite@vku.udn.vn>
2024-10-07 16:41:06 +0700

    Chuong 3

---
+++ Bai19.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Bai19 {
+	public static void main(String[] args) {
+		double i=0;
+		for( int a=0; a<=100; a++) {
+			
+			i=Math.pow(3,a);
+			System.out.print(i+ " ");
+		}
+	}
+}
